e9cb0567d034c40b777ee0aaa9b3f1ac42a16bf6
alena20 <lenanovik98@gmail.com>
2021-08-30 16:52:42 +0300

    commit

---
+++ src/main/java/by/bsuir/spring/dao/PersonDAO.java
@@ -0,0 +1,13 @@
+package by.bsuir.spring.dao;
+
+public class PersonDAO {
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
